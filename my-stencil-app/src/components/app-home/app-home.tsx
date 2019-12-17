4f81ff330fe3bcf148537e201a9368051e32cb32
Søren If Raarup <soren.raarup@if.dk>
2019-12-17 10:53:25 +0100

    const pers: Person is shown

--- my-stencil-app/src/components/app-home/app-home.tsx
+++ my-stencil-app/src/components/app-home/app-home.tsx
@@ -1,5 +1,14 @@
 import { Component, h } from '@stencil/core';
+// Load my-component
 import  'my-stencil-components'
+import { Person } from 'my-stencil-components/dist/types/models/person';
+
+const pers: Person = {
+  email: 'donald.duck@andeby.disney',
+  name: 'Donald Duck',
+  position: '0',
+  photo: null
+}
 
 @Component({
   tag: 'app-home',
@@ -11,7 +20,7 @@ export class AppHome {
   render() {
     return (
       <div class='app-home'>
-        <my-component first="Import Stencil" last="'Don't call me a framework' JS"></my-component>
+        <my-component first="Import Stencil" last="'Don't call me a framework' JS" person={pers}></my-component>
 
         <p>
           Welcome to the Stencil App Starter.
